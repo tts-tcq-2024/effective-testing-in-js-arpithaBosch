83009f83bfe2716ac6a450254e7b9182512cfa63
arpithaP <163737012+arpithaBosch@users.noreply.github.com>
2024-11-02 13:03:12 +0530

    Update tshirts.js

--- tshirts.js
+++ tshirts.js
@@ -10,8 +10,4 @@ function size(cms) {
     }
 }
 
-expect(size(37)).equals('S');
-expect(size(40)).equals('M');
-expect(size(43)).equals('L');
-console.log('All is well (maybe!)');
 module.exports = { size };
