d3f389b8af918cf4510e7bf592a2d5fd5357990c
arpithaP <163737012+arpithaBosch@users.noreply.github.com>
2024-11-02 12:23:20 +0530

    Update alerter.js

--- alerter.js
+++ alerter.js
@@ -24,3 +24,4 @@ alertInCelcius(400.5);
 alertInCelcius(303.6);
 console.log(`${alertFailureCount} alerts failed.`);
 console.log('All is well (maybe!)');
+module.exports = { networkAlertStub, alertInCelcius };
